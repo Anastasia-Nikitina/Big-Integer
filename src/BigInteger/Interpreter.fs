c798a4244ecedf73fc832a35ded22137f5e1e9ff
Anastasia-Nikitina <nasty.nickitina2011@yandex.ru>
2021-11-13 02:38:52 +0300

    fixes for GUI

--- src/BigInteger/Interpreter.fs
+++ src/BigInteger/Interpreter.fs
@@ -6,6 +6,12 @@ open BigInt
 open FSharp.Text.Lexing
 
 let outputBuffer = "print"
+let private newDataToConsole = Event<string>()
+
+let printed = newDataToConsole.Publish
+
+type Dicts = { VariablesDictionary: Dictionary<string, string>; InterpretedDictionary: Dictionary<AST.VName, AST.Expression> }
+
 let rec processExpr (vDict:Dictionary<AST.VName,AST.Expression>) expr =
     match expr with
     | AST.Num n -> n
@@ -25,7 +31,7 @@ let rec processExpr (vDict:Dictionary<AST.VName,AST.Expression>) expr =
     | AST.Abs x -> absNWS (processExpr vDict x)
     | AST.Bin x -> toBin (processExpr vDict x)
 
-let processStmt (vDict:Dictionary<AST.VName,AST.Expression>) (pDict:Dictionary<string,string>) stmt =
+let processStmt (vDict:Dictionary<AST.VName,AST.Expression>) printString stmt =
     match stmt with
     | AST.Print v ->
         let data =
@@ -35,30 +41,40 @@ let processStmt (vDict:Dictionary<AST.VName,AST.Expression>) (pDict:Dictionary<s
             | _ -> failwithf "Variable %A is not declared." v
         match data with
         | AST.Num n ->
-            let num = NWSToString n
-            if pDict.ContainsKey outputBuffer
-            then 
-                pDict.[outputBuffer] <- (pDict.[outputBuffer] + (if num.[0] = '+' then num.[1..] else num) + "\n")
-            else
-                pDict.Add (outputBuffer, (if num.[0] = '+' then num.[1..] else num) + "\n")
-        | _ ->
-            failwithf "Num expected, got: %A" data
+                let num = NWSToString n
+                let str = (if num.[0] = '+' then num.[1..] else num) + "\n"
+                newDataToConsole.Trigger str
+                vDict, printString + str
+            | _ ->
+                failwithf "Num expected, got: %A" data
     | AST.VDecl(v,e) ->
-        if vDict.ContainsKey v
-        then vDict.[v] <- AST.Num (processExpr vDict e)
-        else vDict.Add(v, AST.Num (processExpr vDict e))
-    vDict, pDict
+            if vDict.ContainsKey v
+            then vDict.[v] <- AST.Num (processExpr vDict e)
+            else vDict.Add(v, AST.Num (processExpr vDict e))
+            vDict, printString
 
-let run ast =
+let runVariables (startDicts: Dicts) ast =
+    let startDict = startDicts.VariablesDictionary
+    let variableDict = startDicts.InterpretedDictionary
+    let vD, _ = List.fold (fun (d1, ps) stmt -> processStmt d1 ps stmt) (variableDict, "") ast
+    for i in vD.Keys do
+        match vD.[i] with
+        | AST.Num n ->
+            match i with
+            | AST.Var k -> startDict.[k] <- NWSToString n
+        | _ -> failwith "impossible case"
+    let dicts = { VariablesDictionary = startDict; InterpretedDictionary = vD }
+    dicts
+    
+let runPrint ast =
     let vDict = Dictionary<_,_>()
-    let pDict = Dictionary<_,_>()
     let varDict = Dictionary<_,_>()
-    let vD, _ = List.fold (fun (d1, d2) stmt -> processStmt d1 d2 stmt) (vDict, pDict) ast
+    let vD, printString = List.fold (fun (d1, ps) stmt -> processStmt d1 ps stmt) (vDict, "") ast
     for i in vD.Keys do
         match vD.[i] with
         | AST.Num n -> varDict.[string i] <- NWSToString n
         | _ -> failwith "impossible case"
-    vD, varDict, pDict
+    printString
 
 let calculate (ast:AST.Stmt list) =
     match ast.[0] with
@@ -79,7 +95,7 @@ let parse text =
         let message = errorMsg.Message
         let lastToken = String(lexbuf.Lexeme)
         printf "Parsing failed at: "
-        printfn "line %A, column %A;" (line+1) (column+1)
+        printfn "line %A, column %A;" (line + 1) (column + 1)
         printfn "Last token %A" lastToken
         printfn "Message %A" message
         let errorString = (sprintf "Execution failed at: line %A, column %A. Last token %A" (line + 1) (column + 1) lastToken) 
